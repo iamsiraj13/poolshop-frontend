29f93c3c82bbff45b8ebf2ff3085d60a3aee26c9
iamsiraj13 <iamsiraj13@gmail.com>
2024-11-16 12:27:32 +0600

    project initialize

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import "remixicon/fonts/remixicon.css";
+import router from "./routers/router.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
